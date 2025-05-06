a1674f39ac7a52f8513ea164c7c1f6738c7f0816
joyakinyi <joyakinyi940@gmail.com>
2025-05-06 11:06:10 +0300

    Updated app.py

--- app.py
+++ app.py
@@ -63,5 +63,6 @@ def update_graph(sheet_name):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8050))  # Use $PORT from Render, fallback to 8050 locally
+    app.run(host="0.0.0.0", port=port, debug=True)
 
